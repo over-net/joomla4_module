8ca7aa342e69b49ed3365d2e424cc2857f78c18f
over_net <kulik1979@ukr.net>
2022-11-30 13:57:56 +0200

    ready base frame

---
+++ mod_example.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage mod_example
+ *
+ * @copyright   M.Kulyk
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
---
+++ src/Helper/ExampleHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package         Joomla.Site
+ * @subpackage      mod_example
+ *
+ * @author          M.Kulyk
+ *
+ * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @since
+ */
+
+
+namespace Joomla\Module\Example\Site\Helper;
+
+
+\defined('_JEXEC') or die;
+
+class ExampleHelper
+{
+
+}
