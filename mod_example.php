0bd5570aec089d8a95b63503cae581e92ad91581
over_net <kulik1979@ukr.net>
2022-11-30 14:04:35 +0200

    ready

--- mod_example.php
+++ mod_example.php
@@ -6,3 +6,25 @@
  * @copyright   M.Kulyk
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
+
+defined('_JEXEC') or die;
+
+
+use Joomla\Module\Example\Site\Helper\ExampleHelper;
+
+/**
+ * @var object $params
+ * @var object $module
+ */
+
+try
+{
+	$model = new ExampleHelper();
+}
+catch (Exception $e)
+{
+}
+
+
+require JModuleHelper::getLayoutPath('mod_example', $params->get('layout', 'default'));
